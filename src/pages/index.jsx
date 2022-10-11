4106d9eb9ff938898366134f32dab48dc4243bef
Eniola Ademola <it.eniolaademola@gmail.com>
2022-10-11 21:16:44 +0100

    add default page export

---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+export { default as Contact } from "./contact/Contact";
+export { default as Home } from "./home/Home";
+export { default as Product } from "./products/Product";
+export { default as Services } from "./services/Services";
+export { default as Notfound } from "./notfound/Notfound";
+
+// Service drop down pages
+export { default as Consulting } from "./services/service-pages/consulting/Consulting";
+export { default as Design } from "./services/service-pages/design/Design";
+export { default as Marketing } from "./services/service-pages/marketing/Marketing";
+export { default as Development } from "./services/service-pages/development/Development";
